03c2df397a2ac323464555d435b89bf4c3f06adf
Zoom <egor147536987@gmail.com>
2023-12-22 16:59:40 +0300

    initial

---
+++ index.js
@@ -0,0 +1,86 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '6883028473:AAF6YdnT7IozPSL_l3dbQT0eLCaWGNzd_Gg';
+const WebUrl='https://rococo-wisp-b5b1a7.netlify.app/'
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+const app=express();
+
+app.use(express.json());
+app.use(cors())
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text=msg.text;
+    if (text==='/start'){
+        await bot.sendMessage(chatId,'Down', {
+            reply_markup:{
+                keyboard:[
+                    [{text:'Заполнить форму', web_app: {url: WebUrl + 'form'}}]
+                ]
+            }
+        })
+
+
+        await bot.sendMessage(chatId,'Check up:', {
+            reply_markup:{
+                inline_keyboard:[
+                    [{text:'Make a check up', web_app: {url: WebUrl}}]
+                ]
+            }
+        })
+    }
+
+    if (msg?.web_app_data?.data) {
+        try {
+            const data=JSON.parse(msg?.web_app_data?.data)
+
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country)
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street)
+            await bot.sendMessage(chatId, 'Спасибо за форму!')
+
+            setTimeout(async ()=>{
+                await bot.sendMessage(chatId,'Всю  ифнормацию можно получить по... Никак!');
+            }, 3000)
+
+        }catch(e){
+            console.log(e);
+        }
+
+        
+    }
+});
+
+app.post('/web-data',async (req, res)=>{
+    const {queryId, products, totalPrice}=req.body;
+
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: 'queryId',
+            title: 'Успешная покупка',
+            input_message_content:{message_text:'Поздравляю, вы купили товара на сумму ' + totalPrice}
+        })
+        return res.status(200).json({});
+        
+    } catch (e) {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: 'queryId',
+            title: 'Не удалось приобрести товар',
+            input_message_content:{message_text:'Не удалось приобрести товар'}
+        })
+        return res.status(500).json({});
+
+    }
+})
+
+
+const PORT=3000;
+
+app.listen(PORT, ()=> console.log('server sterted on PORT: ' + PORT))
